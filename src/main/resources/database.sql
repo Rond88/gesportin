cb7c620b5365a2bd825e1b64d146c4e2cecf47d4
rond88 <r0nald19202006@gmail.com>
2026-01-08 09:27:56 +0100

    Corrección de la tabla de la base de datos

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,14 +1,20 @@
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `producto`
+-- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE `producto` (
+CREATE TABLE `usuario` (
   `id` bigint NOT NULL,
-  `nombre` varchar(255) COLLATE utf32_unicode_ci NOT NULL,
-  `precio` decimal(10,0) NOT NULL,
-  `stock` int NOT NULL
+  `nombre` varchar(256) COLLATE utf32_unicode_ci NOT NULL,
+  `apellido1` varchar(256) COLLATE utf32_unicode_ci NOT NULL,
+  `apellido2` varchar(256) COLLATE utf32_unicode_ci NOT NULL,
+  `username` varchar(256) COLLATE utf32_unicode_ci NOT NULL,
+  `password` varchar(256) COLLATE utf32_unicode_ci NOT NULL,
+  `fecha_alta` datetime NOT NULL,
+  `genero` tinyint NOT NULL,
+  `id_tipousuario` bigint NOT NULL,
+  `id_club` bigint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
 --
@@ -16,9 +22,9 @@ CREATE TABLE `producto` (
 --
 
 --
--- Indices de la tabla `producto`
+-- Indices de la tabla `usuario`
 --
-ALTER TABLE `producto`
+ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -26,7 +32,7 @@ ALTER TABLE `producto`
 --
 
 --
--- AUTO_INCREMENT de la tabla `producto`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
-ALTER TABLE `producto`
+ALTER TABLE `usuario`
   MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
