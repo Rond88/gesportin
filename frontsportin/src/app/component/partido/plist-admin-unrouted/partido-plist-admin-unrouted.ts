13e1066618ab3bcbdcc20e3f48290da2d3ef82ec
R0nald-ctrl <r0nald19202006@gmail.com>
2026-02-11 11:55:34 +0100

    partido unrouted por empezar

---
+++ frontsportin/src/app/component/partido/plist-admin-unrouted/partido-plist-admin-unrouted.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partido-plist-admin-unrouted',
+  imports: [],
+  templateUrl: './partido-plist-admin-unrouted.html',
+  styleUrl: './partido-plist-admin-unrouted.css',
+})
+export class PartidoPlistAdminUnrouted {
+
+}
